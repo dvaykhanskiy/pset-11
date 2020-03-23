830f19696ec416b3220b06c179e82b022d9af337
dvaykhanskiy <dvaykhanskiy@ucvts.org>
2020-03-22 20:20:30 -0400

    made starting vars for ex 4

--- src/Exercises.java
+++ src/Exercises.java
@@ -30,13 +30,13 @@ public class Exercises {
   }
 
   public int findMeFaster(ArrayList<Integer> list, int target) {
-    if (list == null || list.size() == 0) {
-          return -1;
-      }
-
       int start = 0;
       int middle = 0;
       int end = list.size() - 1;
+
+      if (list == null || list.size() == 0) {
+            return -1;
+      }
 
       while (start <= end) {
           middle = (start + end) / 2;
@@ -52,7 +52,9 @@ public class Exercises {
   }
 
   public int findMeFaster(String[] list, String target) {
-    return -1;
+    int start = 0;
+    int middle = 0;
+    int end = list.size() - 1;
   }
 
   public int[] bubble(int[] list, boolean ascending) {
