838dcfee5f3afa81b7f6ddde571e464b500725c2
dvaykhanskiy <dvaykhanskiy@ucvts.org>
2020-03-22 21:01:55 -0400

    made ascending code for ex 6

--- src/Exercises.java
+++ src/Exercises.java
@@ -107,7 +107,17 @@ public class Exercises {
       return null;
     }
 
-    
+    if (ascending) {
+      for (int i = 0; i < list.size() -1; i++) {
+        for (int j = 0; j < list.size()-1-i; j++) {
+          if (list.get(j).compareTo(list.get(j+1)) > 0) {
+            String temp = list.get(j);
+            list.set(j, list.get(j+1));
+            list.set(j+1, temp);
+          }
+        }
+      }
+    }
   }
 
   public ArrayList<Integer> insertion(ArrayList<Integer> list, boolean ascending) {
