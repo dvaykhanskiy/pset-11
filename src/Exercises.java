b2743e9e9e73d0c0f185756248e85de5eb5f4c25
dvaykhanskiy <dvaykhanskiy@ucvts.org>
2020-03-22 20:30:25 -0400

    made ascending code for ex 5

--- src/Exercises.java
+++ src/Exercises.java
@@ -78,7 +78,17 @@ public class Exercises {
       return null;
     }
 
-    
+    if (ascending) {
+      for (int i = 0; i < list.length - 1; i++) {
+        for (int j = 0; j < list.length - 1 - i; j++) {
+          if (list[j] > list[j + 1]) {
+            int temp = list[j];
+            list[j] = list[j + 1];
+            list[j+1] = temp;
+          }
+        }
+      }
+    }
   }
 
   public ArrayList<String> bubble(ArrayList<String> list, boolean ascending) {
